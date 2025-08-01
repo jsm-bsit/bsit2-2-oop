7f2ad57470b29084587550c6670871474b7c4fd0
Jose Saturnino Malong <jsmalong02705@liceo.edu.ph>
2025-08-01 09:37:02 +0800

    ACTLAB2

--- bsit2-2-oop/src/Main.java
+++ bsit2-2-oop/src/Main.java
@@ -1,9 +1,26 @@
-
-
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner malongScanner = new Scanner(System.in);
+
+        System.out.println("enter number 1: ");
+        int num1 = malongScanner.nextInt();
+        System.out.println("enter number 2: ");
+        int num2 = malongScanner.nextInt();
+        System.out.println("enter number 3: ");
+        int num3 = malongScanner.nextInt();
+        System.out.println("enter number 4: ");
+        int num4 = malongScanner.nextInt();
+        System.out.println("enter number 5: ");
+        int num5 = malongScanner.nextInt();
+
+        int totalSum = num1 + num2 + num3 + num4 + num5;
+
+        System.out.println("total number is: " + totalSum);
+
+
+
     }
 }
 
