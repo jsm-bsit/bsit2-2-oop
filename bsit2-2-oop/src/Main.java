c3b92da4f40fa95bac47c436dc3c936ea3542523
Jose Saturnino Malong <jsmalong02705@liceo.edu.ph>
2025-08-04 12:03:41 +0800

    WALAPANAHUMAN

--- bsit2-2-oop/src/Main.java
+++ bsit2-2-oop/src/Main.java
@@ -1,9 +1,11 @@
-
-
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
+        System.out.println("Book and their authors");
 
+        String title;
+        String author;
+        int pages = 10;
+        boolean isAvailable = true;
+    }
+
+}
