d59223db8bc639ded3852eff5e0798b18a89b053
Jose Saturnino Malong <jsmalong02705@liceo.edu.ph>
2025-08-18 11:36:18 +0800

    LABACT4

---
+++ GradeCalculator/src/GradeCalculator2.java
@@ -0,0 +1,13 @@
+public class GradeCalculator2{
+    public static void main(String[] args) {
+        GradeCalculator calculator = new GradeCalculator();
+
+        String studentName = "John Smith";
+        double[] grades = {85.5, 92.0, 78.5, 90.0};
+        double average = calculator.calculateAverage(grades);
+
+        String letterGrade = calculator.getLetterGrade(average);
+        calculator.displayResult(studentName, average);
+        calculator.displayResult(studentName, average, letterGrade);
+    }
+}
